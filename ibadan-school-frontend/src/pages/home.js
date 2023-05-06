21a51d5c921f2dcd539287d5284610da5ea12b58
donswicks <donswicks@gmail.com>
2023-05-05 18:47:59 -0700

    Added News And Events Section In The Home Page

--- ibadan-school-frontend/src/pages/home.js
+++ ibadan-school-frontend/src/pages/home.js
@@ -3,24 +3,76 @@ import Header from "../components/header";
 import FixedFooter from "../components/fixedfooter";
 import ImageSlideShow from "../components/imageslideshow";
 import ScrollableFooter from "../components/scrollablefooter";
+import homeStyles from "./styles/home.module.css";
+import { NavLink } from "react-router-dom";
 
 export default class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            news: [],
+            events: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            events: this.eventsCardApi(),
+            news: this.newsCardApi()
+        });
+    }
+
+    eventsCardApi = () => {
+        const content = [];
+
+        for (let i = 0; i < 3; ++i) {
+            content.push(this.createCard(`Event ${i + 1}`, i, "/events"));
+        }
+
+        return content;
+    }
+
+    newsCardApi = () => {
+        const content = [];
+
+        for (let i = 0; i < 3; ++i) {
+            content.push(this.createCard(`News ${i + 1}`, i, "/news"));
+        }
+
+        return content;
+    }
+
+    createCard = (message, key, linkTo) => {
+        return (
+            <NavLink to={linkTo} className={homeStyles.newsEventCard} key={key}>
+                <div className={homeStyles.newsEventDateTime}>
+                    <span> May 05, 2023 </span>
+                    <div> 9:00 AM - 12:00 PM </div>
+                </div>
+                <div className={homeStyles.newsEventMessage}> {message} on on on on on on on on  ... </div>
+            </NavLink>
+        );
+    }
 
     render() {
         return <div>
             <Header />
             <ImageSlideShow />
-
-            <div style={{
-                position: "relative",
-                border: "thin solid #808080", textAlign: "center", fontSize: "2em",
-                width: "fit-content", left: "30%", height: "5em", borderRadius: "0.5em",
-                padding: "2%",
-                boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-            }}>
-                <span style={{ borderBottom: "thin solid #000000" }}> A Center Of Learning For The Hearing-Challenged </span>
+            <div className={homeStyles.storyCard}>
+                <span className={homeStyles.storyMessage}> A Center Of Learning For The Hearing-Challenged </span>
             </div>
+            <div className={homeStyles.newsEventSection}>
+                <div className={homeStyles.newsEventTitle}> Events </div>
+                {this.state.events}
+                <NavLink to="/events"> <div className={homeStyles.newsEventButton}> VIEW ALL EVENTS </div> </NavLink>
+            </div>
+            <div className={homeStyles.newsEventSection}>
+                <div className={homeStyles.newsEventTitle}> News </div>
+                {this.state.news}
+                <NavLink to="/news"><div className={homeStyles.newsEventButton}> VIEW ALL NEWS </div></NavLink>
 
+            </div>
             <ScrollableFooter />
             <FixedFooter />
         </div >;
